1c7be2e197fbd31aa41f530bb4f63d4b0a35dba9
mauromesas <mauromesas98@gmail.com>
2023-11-02 02:13:43 -0300

    Agrego Leccion 10 Java

---
+++ mauro/Java/Leccion 10/EjercicioMatrices02/src/matrices/Ejercicio02.java
@@ -0,0 +1,36 @@
+package matrices;
+
+/*
+    Ejercicio 2 (4* en Clase 12): Crear una matriz de tamaño 7x7 y rellenarla
+    de forma que los elementos de la diagonal principal sean 1 y el resto 0.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int matriz[][] = new int [7][7];
+        
+        // Llenar matriz
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if(i==j){
+                    matriz[i][j] = 1;
+                }
+                else{
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+        // Mostrar matriz
+        System.out.println("Matriz Completa: ");
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+}
