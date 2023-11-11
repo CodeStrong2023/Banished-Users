08415832378e616cc6aa3ec37cf91428a3d48982
mauromesas <mauromesas98@gmail.com>
2023-11-11 00:50:20 -0300

    Agrego Clase 11 de Js

--- mauro/JavaScript/Leccion 3/2. Aclaraciones con let-var-const.js
+++ mauro/JavaScript/Leccion 3/2. Aclaraciones con let-var-const.js
@@ -52,3 +52,53 @@ const fechaNacimiento = 2001;
 console.log(fechaNacimiento);
 //fechaNacimiento = 2003;
 //console.log(fechaNacimiento); // Solo se ejecuta el console anterior
+
+// Clase 11
+
+// Evitar repetir el código
+// Dry don't repeat yourself
+
+let days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+function getDay(n){
+    if(n < 1 || n > 7){
+        throw new Error('out of range');
+    }
+    return days[n-1];
+}
+
+switch (days) {
+    case 'Lunes':
+        console.log('hoy es ' +days);
+        break;
+    case 'Martes':
+        console.log('hoy es ' +days);
+        break;
+    case 'Miercoles':
+        console.log('hoy es '+days);
+        break;
+    case 'Jueves':
+        console. log('hoy es '+days);
+        break;
+    case 'Viernes' :
+        console.log('hoy es '+days);
+    case 'Sabado' :
+        console.log('hoy es '+days);
+    case 'Domingo' :
+        console.log('hoy es '+days);
+    default:
+        break;
+    }
+    
+
+// Version Mejorada
+
+let days2 = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+function getDay(n){
+    if(n < 1 || n > 7){
+        throw new Error('out of range');
+    }
+    return days2[n-1];
+}
+
+console.log(getDay(5));
